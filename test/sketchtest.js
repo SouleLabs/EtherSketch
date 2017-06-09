27271e788af7f1257d537518921a572f113c10f0
Parthasarathy Ramanujam <zincoshine>
2017-06-09 19:46:00 +0530

    Added initial version of testing scripts

---
+++ test/sketchtest.js
@@ -0,0 +1,8 @@
+var EtherSketch = artifacts.require("./EtherSketch.sol");
+var expect = require("chai").expect;
+var sketch;
+contract('EtherSketch', function(accounts) {
+  it("", async function() {
+    //code goes here
+  });
+});
