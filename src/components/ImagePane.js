ce6e860f0df6a78e103ff7260213fb47b80b1f2e
Rafael Pivato <rafael@pivato.info>
2017-06-13 20:30:04 -0700

    Zoom in and out buttons

--- src/components/ImagePane.js
+++ src/components/ImagePane.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Stage, Layer, Image } from "react-konva";
+import FontAwesome from "react-fontawesome";
 import _ from "lodash";
 import "./ImagePane.css";
 
@@ -7,7 +8,8 @@ const url = "http://www.milliondollarhomepage.com/index_files/image-map.png";
 
 class ImagePane extends Component {
   state = {
-    image: null
+    image: null,
+    scale: 1.0
   };
 
   componentDidMount() {
@@ -23,22 +25,24 @@ class ImagePane extends Component {
   /**
     Limits dragging of image so it doesn't go outside container bounds.
 
-    The idea is not to show any blank space without need. The background can
-    still be seen if image and container have some peculiar size combination.
+    The idea is not to let the image to get thrown too much to a corner,
+    neither pulled that much to another.
   */
   get dragBound() {
     return pos => {
       let { x, y } = pos;
       const { container, image } = this.refs;
-      const deltaX = container.offsetWidth - image.width();
-      const deltaY = container.offsetHeight - image.height();
-      if (x > 0) {
-        x = 0;
+      const { scale } = this.state;
+      const easy = 40;
+      const deltaX = container.offsetWidth - image.width() * scale - easy;
+      const deltaY = container.offsetHeight - image.height() * scale - easy;
+      if (x > easy) {
+        x = easy;
       } else if (x < deltaX) {
         x = deltaX;
       }
-      if (y > 0) {
-        y = 0;
+      if (y > easy) {
+        y = easy;
       } else if (y < deltaY) {
         y = deltaY;
       }
@@ -46,10 +50,32 @@ class ImagePane extends Component {
     };
   }
 
+  get zoomInHandler() {
+    return () => {
+      this.setState({ scale: 2.0 });
+    };
+  }
+
+  get zoomOutHandler() {
+    return () => {
+      this.setState({ scale: 1.0 });
+    };
+  }
+
   render() {
+    const { scale } = this.state;
+    const stageScale = { x: scale, y: scale };
     return (
       <div ref="container" className="ImagePane">
-        <Stage width={1000} height={1000}>
+        <div className="ImagePane-Float">
+          <div onClick={this.zoomInHandler}>
+            <FontAwesome name="plus" />
+          </div>
+          <div onClick={this.zoomOutHandler}>
+            <FontAwesome name="minus" />
+          </div>
+        </div>
+        <Stage ref="stage" scale={stageScale} width={1000} height={1000}>
           <Layer>
             <Image
               ref="image"
