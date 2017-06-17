f0dd79464bc4f5acf69ee68bda54dfce47917109
Rafael Pivato <rafael@pivato.info>
2017-06-17 13:49:36 -0700

    Removed redux router bindings

--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
-import { routerReducer } from "react-router-redux";
 import wallReducer from "./util/wallReducer";
 import web3Reducer from "./util/web3Reducer";
 
 const reducer = combineReducers({
-  routing: routerReducer,
   wall: wallReducer,
   web3: web3Reducer
 });
