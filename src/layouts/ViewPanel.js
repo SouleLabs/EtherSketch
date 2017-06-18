a81820f964748be0c08d5b10f7a401d231234099
Rafael Pivato <rafael@pivato.info>
2017-06-17 17:56:54 -0700

    Edit icon to enter edit panel

--- src/layouts/ViewPanel.js
+++ src/layouts/ViewPanel.js
@@ -5,7 +5,7 @@ class ViewPanel extends Component {
   render() {
     return (
       <div className="ViewPanel">
-        <IconLink name="dollar" to="/home/edit" />
+        <IconLink name="pencil" to="/home/edit" />
       </div>
     );
   }
